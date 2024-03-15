2db8ca42f725a6ed575190e9c3b1a9bc7b8a3406
puurun <clematis7593@gmail.com>
2024-03-15 17:02:58 +0900

    Fixed fa, retake responses

--- chatapp/actions/rule/fa.py
+++ chatapp/actions/rule/fa.py
@@ -1,5 +1,5 @@
 from chatapp.responses.select_response import SelectResponse
 
 
-def fa_response(self, slot):
+def fa_response(slot):
     return SelectResponse().get_response("rule/fa_normal")
--- chatapp/actions/rule/retake.py
+++ chatapp/actions/rule/retake.py
@@ -1,6 +1,5 @@
 from chatapp.responses.select_response import SelectResponse
 
 
-def retake_response(self, slot):
+def retake_response(slot):
     return SelectResponse().get_response("rule/retake_normal")
-    return response
